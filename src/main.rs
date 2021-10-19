55a0e48ec78d9d66039502f841878a3f085881e7
Amiel Martin <amiel.martin@gmail.com>
2021-10-18 16:54:26 -0800

    Start formatting and parsing time

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use std::os::unix::net::{UnixListener, UnixStream};
-use std::thread;
 
 use std::io::Read;
 
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
+
+const STATUS_TIME_TO_SECONDS: i64 = 1_000_000_000;
+const STATUS_TIME_TO_MINUTES: i64 = 60_000_000_000;
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Status {
@@ -19,16 +21,54 @@ struct Status {
     n_pomodoros: u8,
 }
 
-fn handle_client(mut stream: UnixStream) {
+impl Status {
+    fn state(&self) -> &str {
+        match self.state {
+            1 => "RUNNING",
+            2 => "BREAKING",
+            3 => "COMPLETE",
+            4 => "PAUSED",
+            _ => "?",
+        }
+    }
+
+    fn remaining_minutes(&self) -> i64 {
+        self.remaining / STATUS_TIME_TO_MINUTES
+    }
+
+    fn remaining_seconds(&self) -> i64 {
+        self.remaining / STATUS_TIME_TO_SECONDS
+    }
+
+    fn remaining_subseconds(&self) -> i64 {
+        self.remaining_seconds() - (self.remaining_minutes() * 60)
+    }
+
+    fn format_remaining(&self) -> String {
+        if self.remaining > 0 {
+            format!(
+                "{:?}:{:?}m",
+                self.remaining_minutes(),
+                self.remaining_subseconds()
+            )
+        } else {
+            format!(
+                "{:?}:{:?}m ago",
+                -self.remaining_minutes(),
+                -self.remaining_subseconds()
+            )
+        }
+    }
+}
+
+fn handle_client(mut stream: UnixStream) -> std::io::Result<()> {
     let mut response = String::new();
-    stream.read_to_string(&mut response);
+    stream.read_to_string(&mut response)?;
 
     let v: Value = match serde_json::from_str(response.as_str()) {
         Ok(v) => v,
         Err(err) => panic!("Error decoding json {:?}", err),
     };
-
-    println!("GOT {:?}", v);
 
     let json = base64::decode(v.as_str().unwrap().to_string()).unwrap();
 
@@ -36,18 +76,20 @@ fn handle_client(mut stream: UnixStream) {
 
     let status: Status = serde_json::from_str(json_str).unwrap();
 
-    println!(" GOT STATUS: {:?}", status);
+    println!("{:?}: {:?}", status.state(), status.format_remaining());
+
+    return Ok(());
 }
 
 fn main() -> std::io::Result<()> {
-    let listener = UnixListener::bind("/Users/amiel/.pomo/pomo.sock")?;
+    let listener = UnixListener::bind("/Users/amiel/.pomo/publish.sock")?;
 
     // accept connections and process them, spawning a new thread for each one
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
                 /* connection succeeded */
-                thread::spawn(|| handle_client(stream));
+                handle_client(stream)?;
             }
             Err(err) => {
                 print!("Error incoming {:?}", err);
@@ -58,15 +100,3 @@ fn main() -> std::io::Result<()> {
     }
     Ok(())
 }
-
-// use std::io::prelude::*;
-// use std::os::unix::net::UnixStream;
-
-// fn main() -> std::io::Result<()> {
-//     let mut stream = UnixStream::connect("/Users/amiel/.pomo/pomo.sock")?;
-//     stream.write_all(b"OMG OMG")?;
-//     let mut response = String::new();
-//     stream.read_to_string(&mut response)?;
-//     println!("{:?}", response);
-//     Ok(())
-// }
